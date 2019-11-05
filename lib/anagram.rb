a21d21a458b07237bd3098008b979d6ea6e8ec95
Jeffrey Brown <brownjs03@gmail.com>
2019-11-05 02:17:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.find_all do |word|
+      if word.split("").sort == @word.split("").sort
+        true
+      end
+    end
+  end
+  
+end
