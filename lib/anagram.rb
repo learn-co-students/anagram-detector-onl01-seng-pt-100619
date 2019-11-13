ecceaa02beb59c6341e9f938f3486aa50d130b4c
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-13 20:38:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,40 +3,40 @@ class Anagram
   
 attr_accessor :word
 
-  def initialize(word)
-    @word = word
-  end 
+def initialize(word)
+  @word = word
+end 
+#create method to split word
+def split_word 
+end 
 
-  def match(anagram)
-  #binding.pry
-  matching_words = []
 
-  #split word
-  word_array = @word.split(//).sort
+#create method to compare("match"} anagram and word which would be inside anagram_list?
+def match(anagram)
+#binding.pry
+anagram_list = []
 
-  #binding.pry
+#split word
+word_array = @word.split(//).sort
 
-      def split_anagram(array)
-        #split anagram
-        array.split("")
+binding.pry
 
-        #sort anagram also 
-        array.collect do |x| x.sort
-        end 
-      array 
-      end 
+#comapre each index in anagram array and see if any match word array
+#comparing whole word_array to each index in anagram array
 
-    #check to see if they are equal
-    if word_array == anagram
 
-    #return all matches if true
+#check to see if they are equal
+if word_array == anagram
 
-    else 
-    #return an empty array if false 
-    matching_words
+#return all matches if true
 
-    end 
-  end
+else 
+#return an empty array if false 
+anagram_list
+
+end 
+end
+end
 
 
 end
