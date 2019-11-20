41047a89752c656847b410f124a3f8774f2099ac
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-11-20 23:41:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
+require 'pry'
+
 # Your code goes here!
+class Anagram
+  attr_accessor :diaper
+  
+  
+  def initialize(diaper)
+    @diaper=diaper
+    # binding.pry
+  end
+  
+  def match(words)
+    words.select do |word|
+      @diaper.split("").sort == word.split("").sort
+    end
+  end
+ 
+end
